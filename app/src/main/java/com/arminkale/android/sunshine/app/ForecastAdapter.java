67a225327580b15bb346850cd22a1bb4cd8e4078
arminkale <arminkale@gmail.com>
2016-01-23 15:53:36 -0500

    Lesson 5: 04 Two Item View Types 1. Added methods getITemViewType and getViewTypeCount and modified method newView to choose the correct layout type in ForecastAdapter.

--- app/src/main/java/com/arminkale/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/arminkale/android/sunshine/app/ForecastAdapter.java
@@ -16,8 +16,22 @@ import com.arminkale.android.sunshine.app.data.WeatherContract;
  * from a {@link android.database.Cursor} to a {@link android.widget.ListView}.
  */
 public class ForecastAdapter extends CursorAdapter {
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
     }
 
     /*
@@ -25,9 +39,17 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
 
-        return view;
+        // Determine layoutId from viewType
+        if (viewType == VIEW_TYPE_TODAY)
+            layoutId = R.layout.list_item_forecast_today;
+        else if (viewType == VIEW_TYPE_FUTURE_DAY)
+            layoutId = R.layout.list_item_forecast;
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
